8d3bf7cf41e42bc03d6d9665ae76af67e2751463
Karan Doshi <karandoshi98@gmail.com>
2021-12-29 23:15:39 +0530

    Updated Model

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import pickle
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 app = Flask(__name__) # initializing a flask app
 
@@ -28,11 +30,27 @@ def index():
                 research=1
             else:
                 research=0
-            filename = 'finalized_model.pickle'
+            filename = 'admission_pred_Elastic_LR_model.pickle'
             loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
             # predictions using the loaded model file
-            prediction=loaded_model.predict([[gre_score,toefl_score,university_rating,sop,lor,cgpa,research]])
-            print('prediction is', prediction)
+
+            df = pd.read_csv('Admission_Prediction.csv')
+            # print("DF loaded")
+            df['GRE Score'] = df['GRE Score'].fillna(df['GRE Score'].mean())
+            df['TOEFL Score'] = df['TOEFL Score'].fillna(df['TOEFL Score'].mean())
+            df['University Rating'] = df['University Rating'].fillna(df['University Rating'].mean())
+            # print("NA values filled with their respective mean")
+            df.drop('Serial No.', axis=1, inplace=True)
+            # print("Column : serial no dropped")
+            y = df['Chance of Admit']
+            df.drop(columns=['Chance of Admit'], inplace=True)
+            x = df
+            sc = StandardScaler()
+            x_sc = sc.fit_transform(x)
+            prediction=loaded_model.predict(sc.transform([[gre_score,toefl_score,university_rating,sop,lor,cgpa,research]]))
+            # print("Test" + str(loaded_model.predict(sc.transform([[337.000000, 118.0, 4.0, 4.5, 4.5, 9.65, 1]]))))
+
+            # print('prediction is', prediction)
             # showing the prediction results in a UI
             return render_template('results.html',prediction=round(100*prediction[0]))
         except Exception as e:
--- train.py
+++ train.py
@@ -1,6 +1,46 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Ridge, Lasso,LassoCV, RidgeCV,LarsCV,ElasticNet,ElasticNetCV,LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas_profiling import ProfileReport
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 import pickle
-from sklearn.metrics import  r2_score
-print('t')
+
+
+df = pd.read_csv('Admission_Prediction.csv')
+# print("DF loaded")
+df['GRE Score'] = df['GRE Score'].fillna(df['GRE Score'].mean())
+df['TOEFL Score'] = df['TOEFL Score'].fillna(df['TOEFL Score'].mean())
+df['University Rating'] = df['University Rating'].fillna(df['University Rating'].mean())
+# print("NA values filled with their respective mean")
+df.drop('Serial No.',axis=1,inplace=True)
+# print("Column : serial no dropped")
+y = df['Chance of Admit']
+df.drop(columns=['Chance of Admit'], inplace = True)
+x = df
+sc = StandardScaler()
+x_sc = sc.fit_transform(x)
+# print("Data is normalized")
+# print("VIF for the dataset")
+# for i in range(x_sc.shape[1]): #no multicollinearity as VIF < 10
+    # print(variance_inflation_factor(x_sc, i), df.columns[i])
+x_train,x_test,y_train,y_test = train_test_split(x_sc,y, test_size=.10, random_state=100)
+
+# print("Running ELasticNET CV")
+elasticcv = ElasticNetCV(alphas=None, cv=10)
+elasticcv.fit(x_train,y_train)
+
+# print("Running Elastic Net")
+elasticLR = ElasticNet(alpha=elasticcv.alpha_,l1_ratio=elasticcv.l1_ratio_)
+elasticLR.fit(x_train,y_train)
+
+# print("Accuracy on test Dataset - ")
+# print(elasticLR.score(x_test,y_test))
+
+pickle.dump(elasticLR,open('admission_pred_Elastic_LR_model.pickle','wb'))
+
+print(elasticLR.predict(sc.transform([[337.000000,118.0,4.0,4.5,4.5,9.65,1]])))
